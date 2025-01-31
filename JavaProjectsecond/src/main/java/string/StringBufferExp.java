65d6a15584d5160e7ccfa6f231dd54f22246d60d
hp <hp@192.168.1.38>
2025-01-31 11:06:55 +0530

    String Buffer changes

---
+++ JavaProjectsecond/src/main/java/string/StringBufferExp.java
@@ -0,0 +1,11 @@
+package string;
+
+public class StringBufferExp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer obj1 =new StringBuffer("Hello");
+		System.out.println(obj1);
+	}
+
+}
