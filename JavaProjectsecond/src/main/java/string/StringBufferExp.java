9900345c8301c3a83ba3257cc33bcfc106dd94a8
hp <hp@192.168.1.38>
2025-02-03 09:46:54 +0530

    String buffer changes

--- JavaProjectsecond/src/main/java/string/StringBufferExp.java
+++ JavaProjectsecond/src/main/java/string/StringBufferExp.java
@@ -2,10 +2,21 @@ package string;
 
 public class StringBufferExp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		StringBuffer obj1 =new StringBuffer("Hello");
+		obj1.insert(5,"Welcome");
 		System.out.println(obj1);
+		obj1.delete(1, 4);
+		System.out.println(obj1);
+		obj1.append("HOME");
+		System.out.println(obj1);
+		obj1.replace(0, 3, "Welcome");
+		System.out.println(obj1);
+		
+		
+		
 	}
 
 }
