fb28f5370f26c2be5d5914b59d7020b3e95a67d6
hp <hp@192.168.1.38>
2025-02-03 10:18:07 +0530

    Single Inheritance example

---
+++ JavaProjectsecond/src/main/java/InheritExam/InheriExamp1.java
@@ -0,0 +1,13 @@
+package InheritExam;
+
+
+ class InheriExamp1 extends ParentA 
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InheriExamp1 obj1=new InheriExamp1();
+		obj1.add();
+	}
+
+}
